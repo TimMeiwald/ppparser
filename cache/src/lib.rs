8c86d6adc8db177c3d9d4dfc327a814530e9cac0
Tim Meiwald <tim.meiwald@gmail.com>
2023-10-10 01:36:13 +0100

    Initial struct of arrays vs array of structs cache

--- cache/src/lib.rs
+++ cache/src/lib.rs
@@ -108,66 +108,71 @@ pub struct ArgCache {
 }
 
 
-// // Can be reused with reinitialize
-// pub struct MyCache2 {
-//     entries: Vec<ArgCache2>, // Start Position encoded in the indexing of the Cache
-// }
+// Can be reused with reinitialize
+pub struct MyCache2 {
+    entries: Vec<ArgCache2>, // Start Position encoded in the indexing of the Cache
+}
 
-// impl Cache for MyCache2 {
+impl Cache for MyCache2 {
     
-//     fn new(size_of_source: u32, number_of_structs: u32) -> MyCache2 {
-//         let mut c = MyCache1 {
-//             entries: Vec::with_capacity(size_of_source as usize),
-//         };
-//         for i in 0..size_of_source+1 {
-//             // Ensures the Vector in Cache is as large as the input source
-//             c.entries.push(ArgCache {
-//                 entries: Vec::with_capacity(number_of_structs as usize),
-//             });
+    fn new(size_of_source: u32, number_of_structs: u32) -> MyCache2 {
+        let mut c = MyCache1 {
+            entries: Vec::with_capacity(size_of_source as usize),
+        };
+        for i in 0..size_of_source+1 {
+            // Ensures the Vector in Cache is as large as the input source
+            c.entries.push(ArgCache {
+                entries: Vec::with_capacity(number_of_structs as usize),
+            });
 
-//             for _j in 0..number_of_structs+1 {
-//                 // Ensures the Vector in ArgCache is as large as the number of structs(Aka possible arguments since each struct implements resolvable, which is known at parser generation time)
-//                 c.entries[i as usize].entries.push((false, u32::MAX));
-//             }
-//         }
+            for _j in 0..number_of_structs+1 {
+                // Ensures the Vector in ArgCache is as large as the number of structs(Aka possible arguments since each struct implements resolvable, which is known at parser generation time)
+                c.entries[i as usize].entries.push((false, u32::MAX));
+            }
+        }
         
-//         return c;
-//         // for every arg cache in c set size to <number_of_structs>
-//     }
+        return c;
+        // for every arg cache in c set size to <number_of_structs>
+    }
 
 
-//     fn push(&mut self, rule: u32, is_true: bool, start_position: u32, end_position: u32) {
-//         let arg_cache: &mut ArgCache2 = &mut self.entries[start_position as usize];
-//         arg_cache.entries[rule as usize] = (is_true, end_position);
-//     }
-//     fn check(&mut self, rule:u32, start_position: u32) -> Option<(bool, u32)> {
-//         let ret: (bool, u32) = self.entries[start_position as usize].entries[rule as usize];
-//         if ret.1 != u32::MAX {
-//             // Result is returned to callee to unwrap
-//             return Some(ret);
-//         } else {
-//             // Tells callee to simply run the actual code instead of using cached value since one does not exist.
-//             return None;
-//         };
-//     }
-//     fn clear(&mut self){
-//         for i in &mut self.entries{
-//             i.entries.clear();
-//         }
-//     }
-//     fn reinitialize(&mut self) {
-//         for i in &mut self.entries {
-//             for j in  0..i.entries.len(){
-//                 i.entries[j] = (false, u32::MAX)
-//             }
-//         }
+    fn push(&mut self, rule: u32, is_true: bool, start_position: u32, end_position: u32) {
+        let arg_cache: &mut ArgCache2 = &mut self.entries[start_position as usize];
+        arg_cache.is_true[rule as usize] = is_true;
+        arg_cache.end_position[rule as usize] = end_position;
+
+    }
+    fn check(&mut self, rule:u32, start_position: u32) -> Option<(bool, u32)> {
+        let is_true: bool= self.entries[start_position as usize].is_true[rule as usize];
+        let end_position: u32= self.entries[start_position as usize].end_position[rule as usize];
+
+        if end_position != u32::MAX {
+            // Result is returned to callee to unwrap
+            return Some((is_true, end_position));
+        } else {
+            // Tells callee to simply run the actual code instead of using cached value since one does not exist.
+            return None;
+        };
+    }
+    fn clear(&mut self){
+        for i in &mut self.entries{
+            i.is_true.clear();
+            i.end_position.clear();
+        }
+    }
+    fn reinitialize(&mut self) {
+        for i in &mut self.entries {
+            for j in  0..i.is_true.len(){
+                i.end_position[j] = u32::MAX
+            }
+        }
 
 
-//     }
-// }
+    }
+}
 
-// // Create 1 per Position in Cache
-// pub struct ArgCache2 {
-//     is_true: Vec<bool>, // Struct type encoded in the position of the entries
-//     end_position: u32,
-// }
+// Create 1 per Position in Cache
+pub struct ArgCache2 {
+    is_true: Vec<bool>, // Struct type encoded in the position of the entries
+    end_position: Vec<u32>,
+}
