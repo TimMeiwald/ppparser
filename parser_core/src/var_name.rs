3f328a6157712d80478120598889d82c0345e0c7
Tim Meiwald <tim.meiwald@gmail.com>
2023-10-17 20:51:04 +0100

    Modified var name to let the cache deal with everything

--- parser_core/src/var_name.rs
+++ parser_core/src/var_name.rs
@@ -9,26 +9,27 @@ pub fn _var_name_kernel(
     position: u32,
     func: fn(&Context, &Source, u32) -> (bool, u32),
 ) -> (bool, u32) {
-    let cached_val: Option<(bool, u32)>;
+    let cached_val: (bool, u32);
     {
         let res = &mut *(context.cache).borrow_mut();
         cached_val = res.check(rule as u32, position, func);
     };
-    match cached_val {
-        Some(cached_val) => {
-            println!("Cached");
-            cached_val
-        }
-        None => {
-            println!("Not cached");
-            let result = func(context, source, position);
-            {
-                let cache = &mut *(context.cache).borrow_mut();
-                cache.push(rule as u32, result.0, position, result.1);
-            }
-            result
-        }
-    }
+    cached_val
+    // match cached_val {
+    //     Some(cached_val) => {
+    //         println!("Cached");
+    //         cached_val
+    //     }
+    //     None => {
+    //         println!("Not cached");
+    //         let result = func(context, source, position);
+    //         {
+    //             let cache = &mut *(context.cache).borrow_mut();
+    //             cache.push(rule as u32, result.0, position, result.1);
+    //         }
+    //         result
+    //     }
+    // }
 }
 
 pub fn _var_name(
