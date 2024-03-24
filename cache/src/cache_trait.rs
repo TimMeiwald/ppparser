b0746f036f85efa3e096b3f96811a2766c273b8b
Tim Meiwald <tim.meiwald@gmail.com>
2024-03-24 14:34:43 +0000

    Removed generic key

--- cache/src/cache_trait.rs
+++ cache/src/cache_trait.rs
@@ -1,19 +1,19 @@
 
 use rules::rules::Rules;
 #[derive(Copy, Clone)]
-pub struct Key<T>(pub T);
+pub struct Key(pub u32);
 // Whatever method the publisher uses to refer to other locations in the tree/stack or whatever is being used for output
 // Up to the publisher. 
 
-impl From<Key<u32>> for u32 {
-    fn from(i: Key<u32>) -> u32 {
+impl From<Key> for u32 {
+    fn from(i: Key) -> u32 {
         i.0
     }
 }
-pub trait Cache<T> {
+pub trait Cache {
     fn new(size_of_source: u32, number_of_structs: u32) -> Self;
-    fn push(&mut self, rule: Rules, is_true: bool, start_position: u32, end_position: u32, reference: Key<T>);
-    fn check(&self, rule: Rules, start_position: u32) -> Option<(bool, u32, Key<T>)>;
+    fn push(&mut self, rule: Rules, is_true: bool, start_position: u32, end_position: u32, reference: Key);
+    fn check(&self, rule: Rules, start_position: u32) -> Option<(bool, u32, Key)>;
     fn clear(&mut self);
     fn reinitialize(&mut self); //Reset state without deallocating memory for reuse.
 }
--- cache/src/my_cache4.rs
+++ cache/src/my_cache4.rs
@@ -1,16 +1,16 @@
 use crate::{cache_trait::Key, Cache};
 use rules::rules::Rules;
 // This cache will completely flatten the cache to see if that improves performance.
-pub struct MyCache4<T: Copy> {
+pub struct MyCache4 {
     end_position: Vec<u32>,
-    indexes: Vec<Key<T>>,
+    indexes: Vec<Key>,
     is_true: Vec<bool>, // Position encoded as start_position*src_length + struct_position // May be slower due to arithmetic who knows
     number_of_structs: u32,
 }
 
-impl<T: Copy> Cache<T> for MyCache4<T> {
+impl Cache for MyCache4 {
     // Try as flat packed data structure. Since using zero to fill didn't seem to make much difference.
-    fn new(size_of_source: u32, number_of_structs: u32) -> MyCache4<T> {
+    fn new(size_of_source: u32, number_of_structs: u32) -> MyCache4 {
         let capacity = (size_of_source + 1) * (number_of_structs + 1);
         let capacity = capacity as usize;
         let mut c = MyCache4 {
@@ -26,22 +26,25 @@ impl<T: Copy> Cache<T> for MyCache4<T> {
         for _i in 0..capacity {
             c.end_position.push(0); // Might be faster? Maybe compiler inlines it? Can't exactly tell needs better performance profiling vs one loop
         }
+        for _i in 0..capacity {
+            c.indexes.push(Key(u32::MAX)); // Might be faster? Maybe compiler inlines it? Can't exactly tell needs better performance profiling vs one loop
+        }
         c
         // for every arg cache in c set size to <number_of_structs>
     }
 
-    fn push(&mut self, rule: Rules, is_true: bool, start_position: u32, end_position: u32, stack_index: Key<T> ) {
+    fn push(&mut self, rule: Rules, is_true: bool, start_position: u32, end_position: u32, stack_index: Key ) {
         let index = (start_position * self.number_of_structs + (rule as u32)) as usize;
         self.is_true[index] = is_true;
         self.end_position[index] = end_position;
         self.indexes[index] = stack_index;
     }
-    fn check(&self, rule: Rules, start_position: u32) -> Option<(bool, u32, Key<T>)> {
+    fn check(&self, rule: Rules, start_position: u32) -> Option<(bool, u32, Key)> {
         let index = (start_position * self.number_of_structs + (rule as u32)) as usize;
         //println!("Index: {:?}, Start_Position: {:?}, Rule: {:?}", index, start_position, rule);
         let is_true: bool = self.is_true[index];
         let end_position: u32 = self.end_position[index];
-        let indexed: Key<T> = self.indexes[index];
+        let indexed: Key = self.indexes[index];
         if end_position != 0 {
             // Result is returned to callee to unwrap
             Some((is_true, end_position, indexed))
