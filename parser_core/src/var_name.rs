3859d1b70132eb6a433df411d35b076f09856681
Tim Meiwald <tim.meiwald@gmail.com>
2024-08-13 14:39:13 +0100

    Working with some cleanup and stuff

--- parser_core/src/var_name.rs
+++ parser_core/src/var_name.rs
@@ -149,29 +149,41 @@ pub fn _var_name_kernel<T: Cache, S: Publisher>(
     let cached_val: Option<(bool, u32, Key)>;
     let temp_key: Option<Key>;
     let curr_key: Key;
+    {
+        let mut tree = context.stack.borrow_mut();
+        let cache = context.cache.borrow_mut();
 
-    let mut tree = context.stack.borrow_mut();
-    let mut cache = context.cache.borrow_mut();
+        cached_val = cache.check(rule, position);
+        temp_key = tree.last_node();
+        curr_key = tree.add_node(rule, position, 0, temp_key, false);
+        tree.set_last_node(Some(curr_key));
 
-    cached_val = cache.check(rule, position);
-    temp_key = tree.last_node();
-    curr_key = tree.add_node(rule, position, 0, temp_key, false);
-    tree.set_last_node(Some(curr_key));
-    let result = match cached_val {
-        Some(cached_val) => {
+        // If cached val exists. We don't use cache because we need to scope borrow_mut correctly
+        if cached_val.is_some() {
+            let cached_val = cached_val.unwrap();
             // Cached Val at Index Key
             let key = cached_val.2;
             // Make cached subtree a child of parent and current node parent of subtree
-            unsafe {
-                tree.connect(temp_key.unwrap_unchecked(), key);
+            match temp_key {
+                None => {}
+                Some(tkey) => {
+                    tree.connect(tkey, key);
+                }
             }
+            let result = (cached_val.0, cached_val.1);
+            tree.set_node_start_position(curr_key, position);
+            tree.set_node_end_position(curr_key, result.1);
+            tree.set_node_result(curr_key, result.0);
+            tree.set_last_node(temp_key);
+            return result;
             // Return Result
-            (cached_val.0, cached_val.1)
-        }
-        None => {
-            // Here we call again and potentially call _var_name again so we need to not have any references anymore.
+        };
+    } // We lose tree and cache scope here.
+    {
+        if cached_val.is_none() {
             let result = func(context, source, position);
-
+            let mut tree = context.stack.borrow_mut();
+            let mut cache = context.cache.borrow_mut();
             match temp_key {
                 None => {}
                 Some(tkey) => {
@@ -181,14 +193,15 @@ pub fn _var_name_kernel<T: Cache, S: Publisher>(
             // Cache Val
             cache.push(rule, result.0, position, result.1, curr_key);
             // Return Result
-            result
+            tree.set_node_start_position(curr_key, position);
+            tree.set_node_end_position(curr_key, result.1);
+            tree.set_node_result(curr_key, result.0);
+            tree.set_last_node(temp_key);
+            return result;
         }
-    };
-    tree.set_node_start_position(curr_key, position);
-    tree.set_node_end_position(curr_key, result.1);
-    tree.set_node_result(curr_key, result.0);
-    tree.set_last_node(temp_key);
-    result
+    }
+    panic!("Should never reach here. Since we test for cached_val is_some and cached_val is_none.
+    The reason why we do it with two if statements is because we need to allow the RefCells to go out of scope!")
 }
 
 pub fn _var_name<T: Cache, S: Publisher>(
