eb01b6f3b22a44086cca7ddc19a41f7e1dfb9839
TimMeiwald <tim.meiwald@gmail.com>
2025-01-11 13:57:26 +0200

    Added some tests

--- parser/tests/tests_parser_basic.rs
+++ parser/tests/tests_parser_basic.rs
@@ -33,6 +33,78 @@ mod tests {
     }
 
     #[test]
+    fn test_square_braces() {
+        let string = "<Spaces> = ['A'..'Z'];".to_string();
+        let src_len = string.len() as u32;
+        let source = Source::new(&string);
+        let position: u32 = 0;
+        let result: (bool, u32);
+        let context = RefCell::new(BasicContext::new(src_len as usize, RULES_SIZE as usize));
+        {
+            let executor = _var_name(Rules::Grammar, &context, grammar);
+            result = executor(Key(0), &source, position);
+        }
+        println!("Result: {:?}", result);
+        //context.borrow().print_cache();
+        //context.borrow().print_publisher();
+        context
+            .into_inner()
+            .get_publisher()
+            .clear_false()
+            .print(Key(0), Some(true));
+        assert_eq!((result.0, result.1), (true, src_len));
+    }
+
+    #[test]
+    fn test_square_braces_in_mixed() {
+        let string = "<Spaces> = (['A'..'Z'], 'Z');".to_string();
+        let src_len = string.len() as u32;
+        let source = Source::new(&string);
+        let position: u32 = 0;
+        let result: (bool, u32);
+        let context = RefCell::new(BasicContext::new(src_len as usize, RULES_SIZE as usize));
+        {
+            let executor = _var_name(Rules::Grammar, &context, grammar);
+            result = executor(Key(0), &source, position);
+        }
+        println!("Result: {:?}", result);
+        //context.borrow().print_cache();
+        //context.borrow().print_publisher();
+        context
+            .into_inner()
+            .get_publisher()
+            .clear_false()
+            .print(Key(0), Some(true));
+        assert_eq!((result.0, result.1), (true, src_len));
+    }
+
+    #[test]
+    fn test_strings() {
+        let string = r#"<Spaces> = (['A'..'Z'], "ZAA");"#.to_string();
+        let src_len = string.len() as u32;
+        let source = Source::new(&string);
+        let position: u32 = 0;
+        let result: (bool, u32);
+        let context = RefCell::new(BasicContext::new(src_len as usize, RULES_SIZE as usize));
+        {
+            let executor = _var_name(Rules::Grammar, &context, grammar);
+            result = executor(Key(0), &source, position);
+        }
+        println!("Result: {:?}", result);
+        //context.borrow().print_cache();
+        //context.borrow().print_publisher();
+        context
+            .into_inner()
+            .get_publisher()
+            .clear_false()
+            .print(Key(0), Some(true));
+        assert_eq!((result.0, result.1), (true, src_len));
+    }
+
+
+
+
+    #[test]
     fn test_dsl_grammar() {
         println!("{:?}", env::current_dir().unwrap());
         let path = "tests/test_data/Grammar.txt";
