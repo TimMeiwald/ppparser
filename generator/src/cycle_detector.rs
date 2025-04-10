25f0ab9da54143b16482e4cc6517e8fefb06b620
TimMeiwald <tim.meiwald@gmail.com>
2025-04-10 17:19:20 +0300

    Removed some prints and added calculator as a test

--- generator/src/cycle_detector.rs
+++ generator/src/cycle_detector.rs
@@ -9,6 +9,7 @@ use crate::{BasicPublisher, Key};
 pub struct LeftRecursionDetector<'a> {
     source: &'a String,
     rules_name_map: HashMap<String, Key>, // Lookup a key for a rule by the name of the rule.
+    left_recursion_rules: HashMap<String, HashSet<String>>
 }
 
 impl<'a> LeftRecursionDetector<'a> {
@@ -16,6 +17,7 @@ impl<'a> LeftRecursionDetector<'a> {
         let mut lr_detector = LeftRecursionDetector {
             source,
             rules_name_map: HashMap::new(),
+            left_recursion_rules: HashMap::new()
         };
         lr_detector.get_rule_keys(tree); // We get the declared rules and their keys
                                          // So we can then lookup the rule in the tree when it's referenced in a different rule
@@ -24,6 +26,9 @@ impl<'a> LeftRecursionDetector<'a> {
     }
     fn print_rules_name_map(&self) {
         println!("{:#?}", self.rules_name_map);
+    }
+    fn print_left_recursive_rules(&self){
+        println!("{:#?}", self.left_recursion_rules);
     }
     fn get_rule_name(&mut self, tree: &BasicPublisher, node: &Node) -> String {
         debug_assert_eq!(node.rule, Rules::LHS);
@@ -39,7 +44,7 @@ impl<'a> LeftRecursionDetector<'a> {
             let child = tree.get_node(*key);
             match child.rule {
                 Rules::Rule => {
-                    //println!("{}", child.get_string(&self.source));
+                    //// println!("{}", child.get_string(&self.source));
                     let lhs = child.get_children()[0];
                     let lhs = tree.get_node(lhs);
                     let rule_name = self.get_rule_name(tree, lhs);
@@ -65,7 +70,7 @@ impl<'a> LeftRecursionDetector<'a> {
                     let lhs = child.get_children()[0];
                     let lhs = tree.get_node(lhs);
                     let parent_rule_name = self.get_rule_name(tree, lhs);
-                    println!("\nRule: {:?}", parent_rule_name);
+                    // println!("\nRule: {:?}", parent_rule_name);
                     let rhs_key = child.get_children()[1];
                     let rhs = tree.get_node(rhs_key);
                     debug_assert_eq!(rhs.rule, Rules::RHS);
@@ -88,6 +93,7 @@ impl<'a> LeftRecursionDetector<'a> {
         // We check if the node is itself a rule, if yes we grab the RHS index not the left most.
         let node = tree.get_node(key);
         let left_most_child: Option<&Key>;
+        
         match node.rule {
             Rules::Rule => {
                 // Since assignment and whitespace are inlined
@@ -102,7 +108,7 @@ impl<'a> LeftRecursionDetector<'a> {
         // If it get's here they're terminals.
         match left_most_child {
             Some(child) => {
-                println!("Going into child of {:?}", node.rule);
+                // println!("Going into child of {:?}", node.rule);
                 self.left_walk_kernel(tree, *child, parent_rule_name, rules_set);
             }
             None => {
@@ -115,20 +121,21 @@ impl<'a> LeftRecursionDetector<'a> {
                         if !rules_set.insert(node.get_string(&self.source)) {
                             // Was already in the list so we can stop because it means
                             // We'll be looping
-                            println!("{:#?}", rules_set);
+                            // println!("{:#?}", rules_set);
+                            self.left_recursion_rules.insert(parent_rule_name, rules_set);
                             return;
                         }
                         let key = self
                             .rules_name_map
                             .get(&node.get_string(&self.source))
                             .expect("The index should exist. If it doesn't the program is broken.");
-                        println!("Jumping to Rule: {:?}", node.rule);
+                        // println!("Jumping to Rule: {:?}", node.rule);
                         // If it's a reference to a rule then we jump to that rule's index and keep recursing.
                         self.left_walk_kernel(tree, *key, parent_rule_name, rules_set);
                     }
                     _ => {
                         // If it's some other terminal type we ignore it since it terminates.
-                        println!("Terminal {:?}", node.rule);
+                        // println!("Terminal {:?}", node.rule);
                     }
                 }
             }
@@ -168,14 +175,54 @@ mod tests {
                 count_lines(&string2, result.1)
             )
         } else {
-            println!("Successfully parsed")
+            // println!("Successfully parsed")
         }
         let source = &String::from(source);
         let tree = context.into_inner();
         let tree = &tree.get_publisher().clear_false();
         let _lr_detector = LeftRecursionDetector::new(tree, source);
+        _lr_detector.print_left_recursive_rules();
         let _f = stdout().flush().expect("Why did it not flush");
-        println!("\n\n\n");
+        // println!("\n\n\n");
+        //lr_detector.print_rules_name_map();
+        //tree.print(Key(0), None);
+        // let src = &String::from(source);
+        // let sym_table = SymbolTable::new(tree, src);
+        // sym_table.print();
+        // let _gen_code = GeneratedCode::new(&sym_table, &tree, src);
+        // _gen_code.print();
+    }
+
+
+    #[test]
+    fn test_calculator() {
+        let path = "../generator/tests/calculator.dsl";
+        let pathbuf = canonicalize(path).expect("If it's moved change the string above");
+        let string = read_to_string(pathbuf).expect("If it's moved change the string above");
+        let string2 = string.clone();
+        let src_len = string.len();
+        let source = Source::new(&string);
+        let position = 0;
+        let context = BasicContext::new(src_len, RULES_SIZE as usize);
+        let context: RefCell<BasicContext> = context.into();
+        let result = grammar(Key(0), &context, &source, position);
+
+        // Checks full file was parsed.
+        if result.1 != string2.len() as u32 {
+            panic!(
+                "Failed to parse grammar due to syntax error on Line: {:?}",
+                count_lines(&string2, result.1)
+            )
+        } else {
+            // println!("Successfully parsed")
+        }
+        let source = &String::from(source);
+        let tree = context.into_inner();
+        let tree = &tree.get_publisher().clear_false();
+        let _lr_detector = LeftRecursionDetector::new(tree, source);
+        _lr_detector.print_left_recursive_rules();
+        let _f = stdout().flush().expect("Why did it not flush");
+        // println!("\n\n\n");
         //lr_detector.print_rules_name_map();
         //tree.print(Key(0), None);
         // let src = &String::from(source);
