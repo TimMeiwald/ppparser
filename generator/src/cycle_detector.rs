2ed4a09db746f033297dda96baf7310f338e4c54
TimMeiwald <tim.meiwald@gmail.com>
2025-07-13 21:46:03 +0300

    checkpoint

--- generator/src/cycle_detector.rs
+++ generator/src/cycle_detector.rs
@@ -35,6 +35,7 @@ struct RuleCallTree {
     rules_rhs_keys: HashMap<String, Key>,
     rules_referenced_by_rule: HashMap<String, HashSet<String>>,
     is_rule_left_recursive: HashMap<String, LeftRecursive>,
+    rules_left_most_rule_refs: HashMap<String, HashSet<String>>,
 }
 impl RuleCallTree {
     fn new(tree: &BasicPublisher, source: &String) -> Self {
@@ -44,6 +45,7 @@ impl RuleCallTree {
             rules_rhs_keys: HashMap::new(),
             rules_referenced_by_rule: HashMap::new(),
             is_rule_left_recursive: HashMap::new(),
+            rules_left_most_rule_refs: HashMap::new(),
         };
         rc_tree.walk_node_children(tree, source, node);
         // println!("Rules -> Keys: {:#?}", rc_tree.rules_keys);
@@ -54,21 +56,78 @@ impl RuleCallTree {
         );
         rc_tree.repeated_loop_over_referenced_rules();
         println!(
-            "Rules -> IsLeftRecursive: {:#?}",
+            "Rules -> IsLeftRecursive First Time: {:#?}",
             rc_tree.is_rule_left_recursive
         );
         rc_tree.get_left_most_called_rules_of_all_rules(tree, source);
+        println!(
+            "Rules -> Left Most Rules: {:#?}",
+            rc_tree.rules_left_most_rule_refs
+        );
+        // If all left most rules are also not left recursive then we know that rule isn't left recursive either.
+        rc_tree.cleanup_left_most_called_rules(tree, source);
+        println!(
+            "Rules -> IsLeftRecursive: {:#?}",
+            rc_tree.is_rule_left_recursive
+        );
         rc_tree
     }
 
+    fn repeated_loop_over_cleanup_left_most_called_rules(
+        &mut self,
+        tree: &BasicPublisher,
+        source: &String,
+    ) {
+        let mut l = usize::MAX;
+        println!(
+            "Length of is_rule_lr: {:?} {:?}",
+            self.is_rule_left_recursive.len(),
+            l != self.is_rule_left_recursive.len()
+        );
+        while l != self.is_rule_left_recursive.len() {
+            // println!("Looped");
+            l = self.is_rule_left_recursive.len();
+            // If the below call adds more rules as left recursive we loop again
+            // If it does not we break.
+            self.cleanup_left_most_called_rules(tree, source);
+        }
+    }
+
+    fn cleanup_left_most_called_rules(&mut self, tree: &BasicPublisher, source: &String) {
+        for (rule_name, left_most_rule_refs) in &self.rules_left_most_rule_refs {
+            let mut is_not_left_recursive = true;
+            for left_most_ref in left_most_rule_refs {
+                match self.is_rule_left_recursive.get(left_most_ref) {
+                    None => {
+                        // Must assume it could be LR
+                        is_not_left_recursive = false;
+                    }
+                    Some(lr) => {
+                        match lr {
+                            LeftRecursive::False => {
+                                // Do nothing
+                            }
+                            _ => {
+                                is_not_left_recursive = false;
+                            }
+                        }
+                    }
+                }
+            }
+            if is_not_left_recursive {
+                self.is_rule_left_recursive
+                    .insert(rule_name.to_string(), LeftRecursive::False);
+            }
+        }
+    }
     fn get_left_most_called_rules_of_all_rules(&mut self, tree: &BasicPublisher, source: &String) {
         let mut rule_strings = Vec::new();
         for (rule, key) in &self.rules_keys {
-            match self.is_rule_left_recursive.get(rule){
+            match self.is_rule_left_recursive.get(rule) {
                 None => {}
                 Some(lr) => {
                     // Ignore rules we already know are NOT left recursive.
-                    if *lr == LeftRecursive::False{
+                    if *lr == LeftRecursive::False {
                         continue;
                     }
                 }
@@ -78,23 +137,100 @@ impl RuleCallTree {
         for rule_name in rule_strings {
             let key = self.rules_rhs_keys[&rule_name];
             let node = tree.get_node(key);
-            self.get_left_most_called_rules_of_rule(rule_name, &node, tree, source);
+            debug_assert!(node.rule == Rules::RHS);
+            let mut left_most_rules: HashSet<String> = HashSet::new();
+            println!("Rule Name: {:?}", rule_name.clone());
+            self.get_left_most_called_rules_of_rule(
+                rule_name.clone(),
+                &node,
+                tree,
+                source,
+                &mut left_most_rules,
+            );
+            self.rules_left_most_rule_refs
+                .insert(rule_name.to_string(), left_most_rules);
         }
     }
 
-    fn get_left_most_called_rules_of_rule(&mut self, rule_name: String, node: &Node, tree: &BasicPublisher, source: &String) {
+    fn get_left_most_called_rules_of_rule(
+        &mut self,
+        rule_name: String,
+        node: &Node,
+        tree: &BasicPublisher,
+        source: &String,
+        left_most_rules: &mut HashSet<String>,
+    ) {
         // For a given rule we need to find the left most rule or rules
         // to investigate if they can then lead back to the original rule
         // Since that's what determines if a rule is left recursive.
         // A minor complication is that ordered choice can both lead to left recursion.
         // Subexpressions are another complication since they can be considered like an anonymous subrule.
         // This is why there can be multiple rules that are the leftmost referenced rule at runtime.
-        println!("Rule Name: {:?}", rule_name);
         let node_children = node.get_children();
+        // println!("Inspecting {:?}", node.rule);
         for child in node_children {
             let child_node = tree.get_node(*child);
-            match child_node.rule
-            
+            match child_node.rule {
+                Rules::Ordered_Choice => {
+                    // println!("In OC");
+                    for child_key in child_node.get_children() {
+                        let child = tree.get_node(*child_key);
+                        // println!("Child: {:?} {:?}", child.rule, child_key);
+                        self.get_left_most_called_rules_of_rule(
+                            rule_name.clone(),
+                            child,
+                            tree,
+                            source,
+                            left_most_rules,
+                        );
+                    }
+                    return;
+                }
+                Rules::Sequence => {
+                    let children = child_node.get_children();
+                    // Sequence must have at least two children
+                    let child = children[0];
+                    let child = tree.get_node(child);
+                    self.get_left_most_called_rules_of_rule(
+                        rule_name.clone(),
+                        child,
+                        tree,
+                        source,
+                        left_most_rules,
+                    );
+                    return;
+                }
+                Rules::Atom
+                | Rules::RHS
+                | Rules::Nucleus
+                | Rules::Subexpression
+                | Rules::Optional
+                | Rules::Zero_Or_More
+                | Rules::One_Or_More
+                | Rules::And_Predicate
+                | Rules::Not_Predicate => {
+                    self.get_left_most_called_rules_of_rule(
+                        rule_name.clone(),
+                        child_node,
+                        tree,
+                        source,
+                        left_most_rules,
+                    );
+                    return;
+                }
+                Rules::Var_Name_Ref => {
+                    let rule_ref = Self::get_rule_ref_name(source, child_node);
+                    // println!(
+                    //     "One Left Most Rule of {:?} is {:?}",
+                    //     rule_ref, child_node.get_string(source)
+                    // );
+                    left_most_rules.insert(rule_ref);
+                    return;
+                }
+                random_ass_rule => {
+                    // println!("Random ass rule: {:?}", random_ass_rule)
+                }
+            }
         }
     }
 
@@ -103,7 +239,7 @@ impl RuleCallTree {
         // Then we keep looping marking all rules that references only rules that reference no rules as not left recursive.
         // Eventually we are left with only rules that reference other rules that reference other rules(sometimes themselves)
         // So they may be left recursive(can also be right recursive though. This is just an initial simple pass).
-        // To get simpler rules out of the way. 
+        // To get simpler rules out of the way.
         let mut l = usize::MAX;
         println!(
             "Length of is_rule_lr: {:?} {:?}",
@@ -288,6 +424,44 @@ mod tests {
         <test_term> = (<test_term>, '+', <test_fact>)/(<test_term>, '-', <test_fact>)/<test_fact>;
 <test_fact> = (<test_fact>, '*', <Num>)/(<test_fact>, '/', <Num>)/<Num>;
 "##
+        .to_string();
+        let string2 = string.clone();
+        let src_len = string.len();
+        let source = Source::new(&string);
+        let position = 0;
+        let context = BasicContext::new(src_len, RULES_SIZE as usize);
+        let context: RefCell<BasicContext> = context.into();
+        let result = grammar(Key(0), &context, &source, position);
+
+        // Checks full file was parsed.
+        if result.1 != string2.len() as u32 {
+            panic!(
+                "Failed to parse grammar due to syntax error on Line: {:?}",
+                count_lines(&string2, result.1)
+            )
+        } else {
+            // println!("Successfully parsed")
+        }
+        let source = &String::from(source);
+        let tree = context.into_inner();
+        let tree = &tree.get_publisher().clear_false();
+        let _lr_detector = LeftRecursionDetector::new(tree, source);
+        let _f = stdout().flush().expect("Why did it not flush");
+        // println!("\n\n\n");
+        //lr_detector.print_rules_name_map();
+        //tree.print(Key(0), None);
+        // let src = &String::from(source);
+        // let sym_table = SymbolTable::new(tree, src);
+        // sym_table.print();
+        // let _gen_code = GeneratedCode::new(&sym_table, &tree, src);
+        // _gen_code.print();
+    }
+
+    #[test]
+    fn test_var_name_lr2() {
+        let string = r##"<test_indirect_three_level_A> = (<test_indirect_three_level_B>, '-', <test_LR_num>) / <test_LR_num>;
+<test_indirect_three_level_B> = <test_indirect_three_level_C>;
+<test_indirect_three_level_C> = <test_indirect_three_level_A>;"##
         .to_string();
         let string2 = string.clone();
         let src_len = string.len();
