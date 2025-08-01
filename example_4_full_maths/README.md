e850603f7ddbf07a05c9205893563993ce918cae
TimMeiwald <tim.meiwald@gmail.com>
2025-08-01 23:16:12 +0300

    Initial example 4 of a full maths parser

---
+++ example_4_full_maths/README.md
@@ -0,0 +1,9 @@
+# Example Grammar 
+https://www.r-5.org/files/books/computers/compilers/writing/Keith_Cooper_Linda_Torczon-Engineering_a_Compiler-EN.pdf
+
+Extends example 3 to do maths including exponents etc. Integer maths only though because supporting floats etc isn't interesting as a test of parsing. 
+cargo run -- -s ./example_4_full_maths/example_4_full_maths.dsl -t ./ -n example_4_full_maths_parser
+
+
+
+## Please note the parser is not automatically regenerated. This still needs to be done with build script ideally.
---
+++ example_4_full_maths/src/test_number.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+mod tests {
+    use core::cell::RefCell;
+    use example_4_full_maths_parser::*;
+    use std::env;
+    use std::fs::{canonicalize, read_to_string};
+
+    use crate::evaluate_tree;
+
+    #[test]
+    fn test_1() {
+        let string = "20".to_string();
+        let src_len = string.len() as u32;
+        let source = Source::new(&string);
+        let position: u32 = 0;
+        let result: (bool, u32);
+        let context = RefCell::new(BasicContext::new(src_len as usize, RULES_SIZE as usize));
+        {
+            let executor = _var_name(Rules::Number, &context, number);
+            result = executor(Key(0), &source, position);
+        }
+        println!("Result: {:?}", result);
+        //context.borrow().print_cache();
+        //context.borrow().print_publisher();
+        let publisher = context.into_inner().get_publisher();
+        publisher.print(Key(0), Some(true));
+        assert_eq!((result.0, result.1), (true, src_len));
+        let result = evaluate_tree(publisher, &string);
+        assert_eq!(result, 20);
+    }
+}
