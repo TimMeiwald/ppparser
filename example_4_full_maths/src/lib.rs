a84390f0520e0eedde91f69d49030ef6ca2e4e69
TimMeiwald <68278799+TimMeiwald@users.noreply.github.com>
2025-08-03 15:42:31 +0300

    Added some tests using other rules to example_4 (#25)
    * Added some tests using other rules to example_4
    
    * Cargo fmt

--- example_4_full_maths/src/lib.rs
+++ example_4_full_maths/src/lib.rs
@@ -560,4 +560,47 @@ mod tests {
         let result = evaluate_tree(publisher, &string);
         assert_eq!(result, 1);
     }
+
+    #[test]
+    fn test_22() {
+        let string = "20/10".to_string();
+        let src_len = string.len() as u32;
+        let source = Source::new(&string);
+        let position: u32 = 0;
+        let result: (bool, u32);
+        let context = RefCell::new(BasicContext::new(src_len as usize, RULES_SIZE as usize));
+        {
+            let executor = _var_name(Rules::Term, &context, term);
+            result = executor(Key(0), &source, position);
+        }
+        println!("Result: {result:?}");
+        //context.borrow().print_cache();
+        //context.borrow().print_publisher();
+        let publisher = context.into_inner().get_publisher();
+        publisher.print(Key(0), Some(true));
+        assert_eq!((result.0, result.1), (true, src_len));
+        let result = evaluate_tree(publisher, &string);
+        assert_eq!(result, 2);
+    }
+    #[test]
+    fn test_29() {
+        let string = "20-10".to_string();
+        let src_len = string.len() as u32;
+        let source = Source::new(&string);
+        let position: u32 = 0;
+        let result: (bool, u32);
+        let context = RefCell::new(BasicContext::new(src_len as usize, RULES_SIZE as usize));
+        {
+            let executor = _var_name(Rules::Expr, &context, expr);
+            result = executor(Key(0), &source, position);
+        }
+        println!("Result: {result:?}");
+        //context.borrow().print_cache();
+        //context.borrow().print_publisher();
+        let publisher = context.into_inner().get_publisher();
+        publisher.print(Key(0), Some(true));
+        assert_eq!((result.0, result.1), (true, src_len));
+        let result = evaluate_tree(publisher, &string);
+        assert_eq!(result, 10);
+    }
 }
