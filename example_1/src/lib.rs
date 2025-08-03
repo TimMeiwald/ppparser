98d4b0f0096947793d2428095df0ad67fbd91ce3
TimMeiwald <tim.meiwald@gmail.com>
2025-08-03 13:26:41 +0300

    Removed dead code warning for example 1 lib since that's expected

--- example_1/src/lib.rs
+++ example_1/src/lib.rs
@@ -1,10 +1,12 @@
 use example_1_parser::*;
 
+#[allow(dead_code)]
 fn evaluate_tree(publisher: BasicPublisher, source: &String) -> i64 {
     let publisher = publisher.clear_false();
     let grammar_node = publisher.get_node(Key(0));
     evaluate_tree_kernel(&publisher, source, grammar_node)
 }
+#[allow(dead_code)]
 fn evaluate_tree_kernel(publisher: &BasicPublisher, source: &String, node: &Node) -> i64 {
     let mut results: Vec<i64> = Vec::new();
     for child_key in node.get_children() {
