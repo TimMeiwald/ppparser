71910ce47d110352009e08855ccab39c59701880
TimMeiwald <tim.meiwald@gmail.com>
2025-08-04 06:17:15 +0300

    Added comment to update_parsers.sh

--- update_parsers.sh
+++ update_parsers.sh
@@ -1,7 +1,6 @@
+cargo run -- -s ./parser/Grammar.dsl -t ./ -n parser # Update Parser of the Parser Generator itself.
 cargo run -- -s ./examples/example_1/example_1.dsl -t ./examples/ -n example_1_parser
 cargo run -- -s ./examples/example_2/example_2.dsl -t ./examples/ -n example_2_parser
 cargo run -- -s ./examples/example_3_basic_maths/example_3_basic_maths.dsl -t ./examples/ -n example_3_basic_maths_parser
 cargo run -- -s ./examples/example_4_full_maths/example_4_full_maths.dsl -t ./examples/ -n example_4_full_maths_parser
-cargo run -- -s ./parser/Grammar.dsl -t ./ -n parser
-
 cargo fmt
