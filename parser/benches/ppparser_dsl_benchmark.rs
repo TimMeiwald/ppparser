697f56bdba66a7d42583ba08b694f3dc0e2255ae
TimMeiwald <tim.meiwald@gmail.com>
2025-08-10 16:45:36 +0300

    Fix for bench

--- parser/benches/ppparser_dsl_benchmark.rs
+++ parser/benches/ppparser_dsl_benchmark.rs
@@ -3,8 +3,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::fs::{canonicalize, read_to_string};
 
 fn test_grammar() {
-    // println!("{:?}", env::current_dir().unwrap());
-    let path = "tests/test_data/Grammar.txt";
+    let path = "./Grammar.dsl";
     let pathbuf = canonicalize(path).expect("If it's moved change the string above");
     let string = read_to_string(pathbuf).expect("If it's moved change the string above");
     let src_len = string.len();
@@ -16,7 +15,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("test pppparser grammar", |b| b.iter(test_grammar));
 }
 pub fn criterion_benchmark_throughput(c: &mut Criterion) {
-    let path = "tests/test_data/Grammar.txt";
+    let path = "./Grammar.dsl";
     let pathbuf = canonicalize(path).expect("If it's moved change the string above");
     let string = read_to_string(pathbuf).expect("If it's moved change the string above");
     let src_len = string.len();
