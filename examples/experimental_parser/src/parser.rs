8e8787691efae67ef2b2c0d3334025e933cfd716
TimMeiwald <tim.meiwald@gmail.com>
2025-12-09 20:29:13 +0200

    Initial experimental parser

---
+++ examples/experimental_parser/src/parser.rs
@@ -0,0 +1,15 @@
+#![allow(non_camel_case_types)] // Generated Code kinda annoying to deal with so w/e
+#![allow(unused_variables)] // Generated Code also, since everything passes stuff
+#![allow(unused_imports)] // Generated Code also, since everything passes stuff
+use crate::*;
+use std::cell::RefCell;
+#[allow(dead_code)]
+pub fn hello_there<T: Context + 'static>(
+    parent: Key,
+    context: &RefCell<T>,
+    source: &Source,
+    position: u32,
+) -> (bool, u32) {
+    let closure_1 = _string_terminal_opt_ascii(b"Hello there!");
+    closure_1(parent, source, position)
+}
